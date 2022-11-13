4bb22b1a11bc1c170f9f4f91a4b8d294a5a25f31
Muzzarino <dvdresources@gmail.com>
2022-11-13 01:50:11 -0800

    Update module.cpp

--- src/module.cpp
+++ src/module.cpp
@@ -341,6 +341,8 @@ KURIBO_MODULE_BEGIN(BETTER_SMS_MODULE_NAME, BETTER_SMS_AUTHOR_NAME, BETTER_SMS_V
         KURIBO_EXPORT_AS(BetterSMS::getScreenOrthoWidth, "getScreenOrthoWidth__9BetterSMSFv");
         KURIBO_EXPORT_AS(BetterSMS::getScreenToFullScreenRatio, "getScreenToFullScreenRatio__9BetterSMSFv");
         KURIBO_EXPORT_AS(BetterSMS::getScreenRatioAdjustX, "getScreenRatioAdjustX__9BetterSMSFv");
+        KURIBO_EXPORT_AS(BetterSMS::getScreenScale, "getScreenScale__9BetterSMSFv");
+        KURIBO_EXPORT_AS(BetterSMS::getCalculatedFovy, "getCalculatedFovy__9BetterSMSFv");
         KURIBO_EXPORT_AS(BetterSMS::getFrameRate, "getFrameRate__9BetterSMSFv");
 
         /* BMG */
