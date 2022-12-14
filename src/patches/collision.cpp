408d7671d5bcf0d7cb3d5b43b8fcd9564b1093b5
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2022-12-14 14:34:24 -0600

    Collision slope fix

--- src/patches/collision.cpp
+++ src/patches/collision.cpp
@@ -1,4 +1,6 @@
 #include <SMS/macros.h>
+#include <SMS/Map/Map.hxx>
+#include <SMS/Player/Mario.hxx>
 
 
 #include "module.hxx"
@@ -10,5 +12,17 @@ SMS_WRITE_32(SMS_PORT_REGION(0x8018D080, 0x80185908, 0, 0), 0x38000600);
 SMS_WRITE_32(SMS_PORT_REGION(0x8018D088, 0x80185910, 0, 0), 0xB01E0000);
 SMS_WRITE_32(SMS_PORT_REGION(0x8018D08C, 0x80185914, 0, 0), 0x60000000);
 SMS_WRITE_32(SMS_PORT_REGION(0x8018D090, 0x80185918, 0, 0), 0x60000000);
+
+static f32 getRoofNoWater(TMap *map, f32 x, f32 y, f32 z, const TBGCheckData **out) {
+    f32 height = y;
+    while (true) {
+        height = map->checkRoof(x, height + 100.0f, z, out);
+        if (!(*out)->isWaterSurface() || (gpMarioAddress->mState & TMario::STATE_WATERBORN) != 0) {
+            break;
+        }
+    }
+    return height;
+}
+SMS_PATCH_BL(SMS_PORT_REGION(0x802573C8, 0, 0, 0), getRoofNoWater);
 
 #endif
