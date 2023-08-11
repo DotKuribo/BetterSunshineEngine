1967e1bf43f6e69b7fbd49761e86a747663df6c6
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-08-11 00:17:00 -0500

    Fix scoped ptr and update interface

--- include/BetterSMS/libs/scoped_ptr.hxx
+++ include/BetterSMS/libs/scoped_ptr.hxx
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <JKernel/JKRHeap.hxx>
+
 template <typename T> class scoped_ptr {
     scoped_ptr()                              = delete;
     scoped_ptr(const scoped_ptr &)            = delete;
@@ -8,7 +10,10 @@ template <typename T> class scoped_ptr {
 
 public:
     explicit scoped_ptr(T *ptr = nullptr) : ptr_(ptr) {}
-    ~scoped_ptr() { delete ptr_; }
+    ~scoped_ptr() {
+        ptr_->~T();
+        JKRHeap::free(ptr_, nullptr);
+    }
 
     void reset(T *p = nullptr) {
         if (ptr_ != p) {
@@ -23,7 +28,6 @@ public:
     T &operator[](size_t i) const { return ptr_[i]; }
 
     operator bool() const { return ptr_; }
-
 
 private:
     T *ptr_;
