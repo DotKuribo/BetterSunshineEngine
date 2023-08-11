6bb63b66515a946b66f61005c909990a97c52358
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2023-08-11 04:10:08 -0500

    Fix shadows when light is at 0, 0, 0

---
+++ src/patches/light.cpp
@@ -0,0 +1,12 @@
+#include "module.hxx"
+#include "settings.hxx"
+
+
+static void safeLightVectorNormalized(const TVec3f &src, TVec3f &dst) {
+    if (BetterSMS::getBugFixesSetting() && src.x == 0.0f && src.y == 0.0f && src.z == 0.0f) {
+        dst = src;
+        return;
+    }
+    PSVECNormalize(src, dst);
+}
+SMS_PATCH_BL(SMS_PORT_REGION(0x80231148, 0, 0, 0), safeLightVectorNormalized);
