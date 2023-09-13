b04e8736bfcbb0e858151022daef501df98885ca
ATollios <adam.tollios@gmail.com>
2023-09-13 18:08:44 -0500

    Revert "MarioModokiTeselafix"
    This reverts commit 855eccbf20e14defb78189a0608a08c341777c3a.

--- src/patches/modokiTeselafix.cpp
+++
@@ -1,5 +0,0 @@
-#include "module.hxx"
-
-
-//ignores a flag test that would usually only be true on git
-SMS_WRITE_32(SMS_PORT_REGION(0x80083ffc, 0, 0, 0), 0x60000000);
--- src/patches/objectfix.cpp
+++ src/patches/objectfix.cpp
@@ -8,5 +8,3 @@ SMS_WRITE_32(SMS_PORT_REGION(0x801b7660, 0, 0, 0), 0x60000000);
 
 //"/scene/mapObj/cogwheel_rope.bti"
 SMS_WRITE_32(SMS_PORT_REGION(0x801b7644, 0, 0, 0), 0x60000000);
-
-
