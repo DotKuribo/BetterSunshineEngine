9f7f9ab98a43ca45b33f648c8b9bd2642a721182
JoshuaMK <60854312+JoshuaMKW@users.noreply.github.com>
2026-04-14 03:44:34 -0500

    Fix jet balloon drawing underneath the timer

--- src/patches/ui.cpp
+++ src/patches/ui.cpp
@@ -9,7 +9,7 @@
 
 #include "module.hxx"
 
-void ensureRedCoinsPanelAdjustOpen(TGCConsole2 *console, int unk_1, s32 time) {
+static void ensureRedCoinsPanelAdjustOpen(TGCConsole2 *console, int unk_1, s32 time) {
     startAppearTimer__11TGCConsole2Fil(console, unk_1, time);
     if (console->mRedCoinPanel->mPane->mIsVisible)
         startAppearRedCoin__11TGCConsole2Fv(console);
@@ -18,7 +18,7 @@ SMS_PATCH_BL(SMS_PORT_REGION(0x8028A188, 0, 0, 0), ensureRedCoinsPanelAdjustOpen
 SMS_PATCH_BL(SMS_PORT_REGION(0x8028EB28, 0, 0, 0), ensureRedCoinsPanelAdjustOpen);
 SMS_PATCH_BL(SMS_PORT_REGION(0x8028EB48, 0, 0, 0), ensureRedCoinsPanelAdjustOpen);
 
-void ensureRedCoinsPanelAdjustClose(TGCConsole2 *console) {
+static void ensureRedCoinsPanelAdjustClose(TGCConsole2 *console) {
     startDisappearTimer__11TGCConsole2Fv(console);
     if (console->mRedCoinPanel->mPane->mIsVisible) {
         console->mRedCoinPanelBack->mOffset.setValue(0x28, 0, 0, 0,
@@ -28,6 +28,20 @@ void ensureRedCoinsPanelAdjustClose(TGCConsole2 *console) {
     }
 }
 SMS_PATCH_BL(SMS_PORT_REGION(0x8028EB58, 0, 0, 0), ensureRedCoinsPanelAdjustClose);
+
+static bool ensureJetBalloonAdjustOpen() {
+    TGCConsole2 *console;
+    SMS_FROM_GPR(30, console);
+
+    if (console->mIsTimerCard) {
+        return true;
+    }
+
+    // Default behavior (might be better to just return false in this case)
+    return console->mJetBalloonCurrent != console->mJetBalloonCoaster;
+}
+SMS_PATCH_BL(SMS_PORT_REGION(0x8014AC0C, 0, 0, 0), ensureJetBalloonAdjustOpen);
+SMS_WRITE_32(SMS_PORT_REGION(0x8014AC14, 0, 0, 0), 0x2C030000);
 
 static SMS_NO_INLINE void changeTextureForShineCounterOnSave_(u32 *card_save, J2DPicture *picture) {
     size_t shines = TFlagManager::smInstance->getFlag(0x40000);
